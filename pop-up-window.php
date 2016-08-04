02af4b409188b9af742ac7d3d91ff92ebed15cdd
Solen Le Roux-Couloigner <solen.leroux.c@gmail.com>
2016-08-04 11:58:50 +0200

    Idem pour le popup

--- pop-up-window.php
+++ pop-up-window.php
@@ -3,12 +3,16 @@
         <div id="mybox_conteneur">
             <div id="mybox_relative">
                 <div id="mybox_close">
-                    <a class="close" > <p><img src="img/icon-back.png" alt="" width="25px"></p> </a>
+                    <a class="close" >
+                        <p>
+                            <img src="img/icon-back.png" alt="" width="25px">
+                        </p>
+                    </a>
                 </div>
                 <img id="logo" src="img/logo.png" alt="">
                 <p class="texte">EM<span class="texte-a">A</span>IL</p>
                 <h2 class="texte-promo">
-                    <span class="english">Get 20% off your first order <br />no spam we promiss ;-)</span>
+                    <span class="english">Get 20% off your first order <br />No spam !</span>
                     <span class="francais">Profitez de 20% sur votre première commande<br />Promis pas de spam !</span>
                 </h2>
                 <div id="form">
@@ -17,9 +21,7 @@
                             <span class="english"><input type="email" name="email" id="email" placeholder="Your email" /></span>
                             <span class="francais"><input type="email" name="email" id="email" placeholder="Votre mail" /></span>
                         </p>
-                        <a href="#">
-                            <img id="bouton_envoyer" src="img/logo-submit.png" width="50px">
-                        </a>
+                        <input type="submit" value="" id="bouton_envoyer">
                     </form>
                 </div>
             </div>
