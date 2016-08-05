fe80b7f8977fb8b7330cd9390915ac041161e15d
Solen Le Roux-Couloigner <solen.leroux.c@gmail.com>
2016-08-05 16:38:25 +0200

    Cookies pour popup

--- cookies.php
+++ cookies.php
@@ -1,3 +1,5 @@
+<!-- Cookie contenant la langue de l'utilisateur -->
+
 <?php
 // Regarde si un des boutons langue vient d'être actionné
 if (ISSET($_POST["language"])) {
@@ -13,6 +15,11 @@ else {
 }
 // Actualise le cookie avec la langue choisie (doit apparaître sur chaque page)
 setcookie("language", $language, time() + 365*24*3600, null, null, false, true); ?>
+
+
+<!-- Cookie pour que le popup ne réapparaisse pas avant un certain temps (ici, une heure, soit 3600s) -->
+<?php setcookie("popup", "no", time() + 3600, null, null, false, true); ?>
+
 
 <!-- Actualise la page -->
 <?php $currentpage = $_SERVER['REQUEST_URI']; // trouve la page actuelle
