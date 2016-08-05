7fc8cd0fa0e53b17e71017b53247caed0b390393
Solen Le Roux-Couloigner <solen.leroux.c@gmail.com>
2016-08-05 14:37:23 +0200

    Le site est bilingue

--- cookies.php
+++ cookies.php
@@ -1,13 +1,22 @@
 <?php
-if (ISSET($_COOKIE["language"])) {
-    $language = htmlspecialchars($_COOKIE["language"]);
-}
-elseif (ISSET($_POST["language"])) {
+// Regarde si un des boutons langue vient d'être actionné
+if (ISSET($_POST["language"])) {
     $language = htmlspecialchars($_POST["language"]);
 }
+// Sinon, regarde s'il y avait déjà un cookie
+elseif (ISSET($_COOKIE["language"])) {
+    $language = htmlspecialchars($_COOKIE["language"]);
+}
+// Sinon, la langue par défaut sera l'anglais
 else {
     $language = 'english';
 }
+// Actualise le cookie avec la langue choisie (doit apparaître sur chaque page)
 setcookie("language", $language, time() + 365*24*3600, null, null, false, true); ?>
 
-<?php echo $_COOKIE["language"]; ?>
+<!-- Actualise la page -->
+<?php $currentpage = $_SERVER['REQUEST_URI']; // trouve la page actuelle
+$currentpage = substr($currentpage, 18); // supprime les 18 premiers caractères
+if ($currentpage == "cookies.php") {
+    header ("Location: $_SERVER[HTTP_REFERER]" );
+} ?>
--- customJS/site-bilingue.js
+++ customJS/site-bilingue.js
@@ -1,58 +1,9 @@
-// JS pour faire un site bilingue
-// pompé sur thesitewizard.com
-var style_cookie_name = "language" ;
-var style_cookie_duration = 3000 ;
-var style_domain = "www.shapeheart.com" ;
-
-// You do not need to customise anything below this line
-function switch_style ( css_title )
-{
-    // You may use this script on your site free of charge provided
-    // you do not remove this notice or the URL below. Script from
-    // http://www.thesitewizard.com/javascripts/change-style-sheets.shtml
-    var i, link_tag ;
-    for (i = 0, link_tag = document.getElementsByTagName("link") ;
-         i < link_tag.length ; i++ ) {
-        if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
-            link_tag[i].title) {
-            link_tag[i].disabled = true ;
-            if (link_tag[i].title == css_title) {
-                link_tag[i].disabled = false ;
-            }
-        }
-        set_cookie( style_cookie_name, css_title,
-            style_cookie_duration, style_domain );
-    }
+var xhttp;
+if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+} else {
+    // code for IE6, IE5
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
-function set_style_from_cookie()
-{
-    var css_title = get_cookie( style_cookie_name );
-    if (css_title.length) {
-        switch_style( css_title );
-    }
-}
-function set_cookie ( cookie_name, cookie_value,
-                      lifespan_in_days, valid_domain )
-{
-    // http://www.thesitewizard.com/javascripts/cookies.shtml
-    var domain_string = valid_domain ?
-        ("; domain=" + valid_domain) : '' ;
-    document.cookie = cookie_name +
-        "=" + encodeURIComponent( cookie_value ) +
-        "; max-age=" + 60 * 60 *
-        24 * lifespan_in_days +
-        "; path=/" + domain_string ;
-}
-function get_cookie ( cookie_name )
-{
-    // http://www.thesitewizard.com/javascripts/cookies.shtml
-    var cookie_string = document.cookie ;
-    if (cookie_string.length != 0) {
-        var cookie_value = cookie_string.match (
-            '(^|;)[\s]*' +
-            cookie_name +
-            '=([^;]*)' );
-        return decodeURIComponent ( cookie_value[2] ) ;
-    }
-    return '' ;
-}
+document.cookie = "hello=you";
+alert(document.cookie("language"));
